0f3ea2519fdf6aad0f4bba9631d7aaeb7811c944
lijiayu980606 <lijiayu0017@163.com>
2019-08-26 23:49:47 +0800

    Completed Level 2: Add, List

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,7 +1,11 @@
 package seedu.duke;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    //level 1
+    /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -23,6 +27,42 @@ public class Duke {
             }else{
                 System.out.println("    --------------------------------------------------------");
                 System.out.println("    "+command);
+                System.out.println("    --------------------------------------------------------");
+            }
+        }
+    }*/
+    //level 2
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("    --------------------------------------------------------");
+        System.out.println("    Hello! I'm Duke\n" + "    What can I do for you?");
+        System.out.println("    --------------------------------------------------------");
+        List<String> listCommand = new ArrayList<String>();
+        while(true){
+            Scanner myObj = new Scanner(System.in);
+            String command = myObj.nextLine();
+            if(command.equals("bye")){
+                System.out.println("    --------------------------------------------------------");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    --------------------------------------------------------");
+                break;
+            }else if(command.equals("list")){
+                System.out.println("    --------------------------------------------------------");
+                int sizeCommand = listCommand.size();
+                for(int i = 0; i < sizeCommand; i++){
+                    int num=i+1;
+                    System.out.println("    "+ num + ". " + listCommand.get(i));
+                }
+                System.out.println("    --------------------------------------------------------");
+            }else{
+                System.out.println("    --------------------------------------------------------");
+                listCommand.add(command);
+                System.out.println("    added: "+ command);
                 System.out.println("    --------------------------------------------------------");
             }
         }
