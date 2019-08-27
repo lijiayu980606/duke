e8a62b79be59f55a5c48a3b185ec5752954ee12d
lijiayu980606 <lijiayu0017@163.com>
2019-08-27 20:32:23 +0800

    Completed Level-4: ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,72 +4,7 @@ import java.util.Scanner;
 
 
 public class Duke {
-    //level 1
-    /*
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("    --------------------------------------------------------");
-        System.out.println("    Hello! I'm Duke\n" + "    What can I do for you?");
-        System.out.println("    --------------------------------------------------------");
-        while(true){
-            Scanner myObj = new Scanner(System.in);
-            String command = myObj.nextLine();
-            if(command.equals("bye")){
-                System.out.println("    --------------------------------------------------------");
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    --------------------------------------------------------");
-                break;
-            }else{
-                System.out.println("    --------------------------------------------------------");
-                System.out.println("    "+command);
-                System.out.println("    --------------------------------------------------------");
-            }
-        }
-    }*/
-    //level 2
-    /*
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("    --------------------------------------------------------");
-        System.out.println("    Hello! I'm Duke\n" + "    What can I do for you?");
-        System.out.println("    --------------------------------------------------------");
-        List<String> listCommand = new ArrayList<String>();
-        while(true){
-            Scanner myObj = new Scanner(System.in);
-            String command = myObj.nextLine();
-            if(command.equals("bye")){
-                System.out.println("    --------------------------------------------------------");
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    --------------------------------------------------------");
-                break;
-            }else if(command.equals("list")){
-                System.out.println("    --------------------------------------------------------");
-                int sizeCommand = listCommand.size();
-                for(int i = 0; i < sizeCommand; i++){
-                    int num=i+1;
-                    System.out.println("    "+ num + ". " + listCommand.get(i));
-                }
-                System.out.println("    --------------------------------------------------------");
-            }else{
-                System.out.println("    --------------------------------------------------------");
-                listCommand.add(command);
-                System.out.println("    added: "+ command);
-                System.out.println("    --------------------------------------------------------");
-            }
-        }
-    }
-    */
-    //Level 3
+    //level 4
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -83,32 +18,66 @@ public class Duke {
         List<Task> listCmd = new ArrayList<Task>();
         while(true){
             Scanner myObj = new Scanner(System.in);
-            String command = myObj.nextLine();
-            Scanner scCmd = new Scanner(command);
-            if(command.equals("bye")){
+            String ipt = myObj.nextLine();
+            Scanner scCmd = new Scanner(ipt);
+            String type = scCmd.next();
+            if(type.equals("bye")){
                 System.out.println("    --------------------------------------------------------");
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("    --------------------------------------------------------");
                 break;
-            }else if(command.equals("list")){
+            }else if(type.equals("list")){
                 System.out.println("    --------------------------------------------------------");
                 int sizeCommand = listCmd.size();
                 for(int i = 0; i < sizeCommand; i++){
                     int num=i+1;
-                    System.out.println("    "+ num + ".[" + listCmd.get(i).getStatusIcon()+"] "+listCmd.get(i).description);
+                    if(listCmd.get(i).type.equals("T")){
+                        System.out.println("    "+ num + ".[" +listCmd.get(i).type+"]"+" ["+ listCmd.get(i).getStatusIcon()+"] "+listCmd.get(i).description);
+                    }else if(listCmd.get(i).type.equals("D")){
+                        System.out.println("    "+ num + ".[" +listCmd.get(i).type+"]"+" ["+ listCmd.get(i).getStatusIcon()+"] "+listCmd.get(i).description+" (by: "+listCmd.get(i).period+")");
+                    }else{
+                        System.out.println("    "+ num + ".[" +listCmd.get(i).type+"]"+" ["+ listCmd.get(i).getStatusIcon()+"] "+listCmd.get(i).description+" (at: "+listCmd.get(i).period+")");
+                    }
                 }
                 System.out.println("    --------------------------------------------------------");
-            }else if (scCmd.next().equals("done")) {
+            }else if (type.equals("done")) {
                 System.out.println("    --------------------------------------------------------");
                 //System.out.println(scCmd.next());
                 int i =Integer.parseInt(scCmd.next())-1;
                 listCmd.get(i).markAsDone();
                 System.out.println("    Nice! I've marked this task as done:\n"+"    [" + listCmd.get(i).getStatusIcon()+"] "+listCmd.get(i).description);
                 System.out.println("    --------------------------------------------------------");
-            }else{
+            }else if(type.equals("todo")) {
                 System.out.println("    --------------------------------------------------------");
-                listCmd.add(new Task(command));
-                System.out.println("    added: "+ command);
+                Task todo = new Task(scCmd.nextLine());
+                listCmd.add(todo);
+                System.out.println("    Got it. I've added this task: ");
+                System.out.println("    [T][\u2718]"+ todo.description);
+                System.out.println("    Now you have "+ listCmd.size()+" tasks in the list.");
+                System.out.println("    --------------------------------------------------------");
+            }else if(type.equals("deadline")) {
+                System.out.println("    --------------------------------------------------------");
+                String ddlCmd = scCmd.nextLine();
+                Scanner ddlSc = new Scanner(ddlCmd).useDelimiter("\\s*/by\\s*");
+                Task ddl = new Task(ddlSc.next());
+                ddl.setPeriod(ddlSc.next());
+                ddl.taskDeadline();
+                listCmd.add(ddl);
+                System.out.println("    Got it. I've added this task: ");
+                System.out.println("    [D][\u2718]"+ ddl.description+" (by: "+ddl.period+")");
+                System.out.println("    Now you have "+ listCmd.size()+" tasks in the list.");
+                System.out.println("    --------------------------------------------------------");
+            }else if(type.equals("event")){
+                System.out.println("    --------------------------------------------------------");
+                String evtCmd = scCmd.nextLine();
+                Scanner evtSc = new Scanner(evtCmd).useDelimiter("\\s*/at\\s*");
+                Task event = new Task(evtSc.next());
+                event.setPeriod(evtSc.next());
+                event.taskEvent();
+                listCmd.add(event);
+                System.out.println("    Got it. I've added this task: ");
+                System.out.println("    [E][\u2718]"+ event.description +" (at: "+event.period+")");
+                System.out.println("    Now you have "+ listCmd.size()+" tasks in the list.");
                 System.out.println("    --------------------------------------------------------");
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
-public class Task {
+//level3
+/*public class Task {
     protected String description;
     protected boolean isDone;
 
@@ -12,6 +13,37 @@ public class Task {
     }
     public void markAsDone(){
         this.isDone = true;
+    }
+
+}*/
+//level4
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    protected String type;
+    protected String period;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        this.type = "T";
+        this.period=null;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+    public void markAsDone(){
+        this.isDone = true;
+    }
+    public void taskDeadline(){
+        this.type="D";
+    }
+    public void taskEvent(){
+        this.type="E";
+    }
+    public void setPeriod(String timeString){
+        this.period=timeString;
     }
 
 }
